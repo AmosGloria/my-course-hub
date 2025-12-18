17dcd1e82ab54b14a1e7c6aae4cde270fbc361bb
Amos Gloria <amoskesegloria.com>
2025-12-18 16:36:22 +0100

    Fix: update course-info file names

--- src/Components/course-info/CourseInfo.jsx
+++ src/Components/course-info/CourseInfo.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
-import './CourseInfo.css'
+import './CourseInfo.css';
 
-export default function CourseInfo(
-    {
-    img,
-    heading,
-    description,
-    variant,
-    className="",
-    }
-) {
+export default function CourseInfo({
+  img,
+  heading,
+  description,
+  variant,
+  className = "",
+}) {
   return (
     <>
-        <div className={`course-info-container course-info-container--${variant} ${className}`}>
-      <div className='course-info-container__img-container course-info-container__img-container--primary'>
-          <img
-        src={img}
-        alt=""
-        />
-      </div>
-        <h1>{heading}</h1>
-        <p className='course-info-container__description'>{description}</p>
-        
+      <div className={`course-info-container course-info-container--${variant} ${className}`}>
+        <div className="course-info-container_img-container course-info-container_img-container--primary">
+          <img src={img} alt="" />
         </div>
+        <h1>{heading}</h1>
+        <p className="course-info-container__description">{description}</p>
+      </div>
     </>
-  )
+  );
 }
